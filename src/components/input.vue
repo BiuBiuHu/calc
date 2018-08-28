ceaee348e109ff5d4150f8fdda3aded20441d159
yuesh <yueshenghu@ascs.tech>
2018-08-28 13:59:23 +0800

    feat: 优化交互，完善逻辑

--- src/components/input.vue
+++ src/components/input.vue
@@ -2,35 +2,40 @@
     <div>
         <label>
             <span>{{data.name}}</span>
-            <input :placeholder = "data.name" :value = "data.value" @click="getValue"/>
+            <input :placeholder = "data.placeholder" type="Number" v-model = "data.value" v-focus  @blur="getValue"/>
             <span>{{data.total}}</span>
         </label>
     </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {}
-    },
-    props:{
-      data:{
-        type:Object,
-        default:function () {
-          return {
-            name:"default",
-            value:"",
-            total:'0'
-          };
-        }
-      },
-      methods:{
-        getValue(){
-          console.log(this.$props.data.value);
-          this.$emit('newValue',this.$props.data.value);
-        }
+export default {
+  data() {
+    return {};
+  },
+  computed:{
+    value(){
+      return this.$props.data.value
+    }
+  },
+  props: {
+    data: {
+      type: Object,
+      default: function() {
+        return {
+          name: "default",
+          value: "",
+          total: "0"
+        };
       }
     }
-
-
-  }
+  },
+  methods: {
+    getValue() {
+      this.$emit("newValue", this.$props.data);
+    }
+  },
+  mounted() {
+    
+  },
+};
 </script>
