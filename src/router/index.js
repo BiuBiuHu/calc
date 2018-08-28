66c057771c28196e7934b58c73e5e8e69649a63f
yuesh <yueshenghu@ascs.tech>
2018-08-28 14:31:57 +0800

    refactor: 修改文件名

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import test from '@/components/test'
+import calc from '@/components/calc'
 
 Vue.use(Router)
 
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'test',
-      component: test
+      component: calc
     }
   ]
 })
